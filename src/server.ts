1feb6a0351426216b4e3a5f17f820145bff1adaf
conkur <44420386+conkur@users.noreply.github.com>
2023-10-19 10:07:17 -0400

    refactor(server): renames bot

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import * as Express from 'express';
 import {applyMiddlewares} from './middlewares';
-import {RobotSlave} from './bot';
+import {Robot} from './bot';
 
 const app: Express.Application = Express();
 applyMiddlewares(app);
@@ -11,10 +11,10 @@ app.get('/', (_req: Express.Request, res: Express.Response) => {
 
 // GroupMe sends a post request if a chat was sent to the group.
 app.post('/', (req: Express.Request, res: Express.Response) => {
-    robotSlave.readMessage(req.body, req.useragent ? req.useragent.source : null);
+    robot.readMessage(req.body, req.useragent?.source ?? null);
     res.status(200).json({});
 });
 
-const robotSlave = new RobotSlave();
+const robot = new Robot();
 
 export default app;
